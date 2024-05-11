c183aa472bea0a2bf3875f95d7ea71932f362700
Ivan Izhkov <ivan.izhkov@mail.ru>
2024-05-11 17:36:51 +0500

    create form and scripts

--- js/app.js
+++ js/app.js
@@ -42,29 +42,107 @@ buttons.addEventListener('click', (e) => {
   }
 })
 
-// Reviews loading
+// Lazy Loading Reviews
 
 const reviews = document.querySelectorAll('.reviews__item')
-const reviewsButton = document.querySelector('.review__button')
+const loadingButton = document.querySelector('.loading__button')
 
 let count = reviews.length
 let start = 2
 let show = 4
 
-reviewsButton.addEventListener('click', (e) => {
+loadingButton.addEventListener('click', (e) => {
   e.preventDefault()
   let hideReviews = document.querySelectorAll('[data="hiden"]')
 
   for (let i = 0; i < start; i++) {
-    if (show < reviews.length) {
+    if (show < count) {
       let block = hideReviews[i]
       block.removeAttribute('data')
       show++
       block.classList.remove('d-none')
     }
 
-    if (show === reviews.length) {
-      reviewsButton.classList.add('d-none')
+    if (show === count) {
+      loadingButton.classList.add('d-none')
     }
   }
+})
+
+// Lazy Loading Images
+
+const allImages = document.querySelectorAll('.gallery__item')
+const loadImgButton = document.querySelector('#img__button')
+
+let countImages = allImages.length
+let startImages = 3
+let showImages = 6
+
+loadImgButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  let hideImg = document.querySelectorAll('.gallery__item.d-none')
+
+  for (let i = 0; i < startImages; i++) {
+    if (showImages < countImages) {
+      let images = hideImg[i]
+      images.id = ''
+      images.classList.remove('d-none')
+      showImages++
+    }
+
+    if (showImages === countImages) {
+      loadImgButton.classList.add('d-none')
+    }
+  }
+})
+
+// Form
+
+const formChoice = document.querySelector('.form_choice')
+const choiceItems = document.querySelectorAll('.form_choice ul li')
+const choiceArea = document.querySelector('.choice__area')
+const phoneInput = document.querySelector('[type="tel"]')
+const emailInput = document.querySelector('[type="email"]')
+
+formChoice.addEventListener('click', (e) => {
+  e.preventDefault()
+  choiceItems.forEach((item) => {
+    item.classList.remove('selected')
+  })
+  e.target.classList.add('selected')
+})
+
+// Input Phone Mask
+
+const formBtn = document.querySelector('.form_btn--send')
+
+const phoneMask = new IMask(phoneInput, {
+  mask: '+{7}(000)000-00-00',
+})
+phoneInput.addEventListener('input', () => {
+  if (phoneMask.masked.isComplete) {
+    formBtn.classList.add('active')
+  } else {
+    formBtn.classList.remove('active')
+  }
+})
+
+// Smooth Scroll with JQuery :-/
+
+$(function () {
+  $('[data-goto]').on('click', function (event) {
+    event.preventDefault()
+    navBurger.classList.remove('active')
+    nav.classList.remove('active')
+
+    let elementId = $(this).data('goto')
+    let elementOffset = $(elementId).offset().top
+
+    $('html, body').animate(
+      {
+        scrollTop: elementOffset,
+      },
+      1000
+    )
+  })
 })
