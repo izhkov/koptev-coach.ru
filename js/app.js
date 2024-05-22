b4777d9ec4078d94cb7dc0bddcb0cd3e8d0dbb45
Ivan Izhkov <ivan.izhkov@mail.ru>
2024-05-22 14:33:46 +0500

    upgrade gallery and mobile ver

--- js/app.js
+++ js/app.js
@@ -176,3 +176,44 @@ $(function () {
 const footerCopyright = document.querySelector('.footer__copyright')
 const fullYear = new Date().getFullYear()
 footerCopyright.textContent = `© ${fullYear}`
+
+// Animation
+
+const animItems = document.querySelectorAll('.animItems')
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll)
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index]
+
+      const animItemHeight = animItem.offsetHeight
+      const animItemOffset = offset(animItem).top
+      const animStart = 4
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - innerHeight / animStart
+      }
+      if (
+        pageYOffset > animItemOffset - animItemPoint &&
+        pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add('active')
+      } else {
+        if (!animItem.classList.contains('anim-no-hide')) {
+          animItem.classList.remove('active')
+        }
+      }
+    }
+  }
+
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    return { top: rect.top + scrollTop }
+  }
+  setTimeout(() => {
+    animOnScroll()
+  }, 300)
+}
